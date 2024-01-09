900ae74434e314a689a86c76ef28d225dd834131
lirui <821342264@qq.com>
2024-01-09 22:30:27 +0800

    lab6

--- kernel/proc.h
+++ kernel/proc.h
@@ -104,3 +104,23 @@ struct proc {
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
 };
+
+// Saved registers for thread context switches. - lab7-1
+struct ctx {
+    uint64 ra;
+    uint64 sp;
+
+    // callee-saved
+    uint64 s0;
+    uint64 s1;
+    uint64 s2;
+    uint64 s3;
+    uint64 s4;
+    uint64 s5;
+    uint64 s6;
+    uint64 s7;
+    uint64 s8;
+    uint64 s9;
+    uint64 s10;
+    uint64 s11;
+};
--- user/uthread.c
+++ user/uthread.c
@@ -14,7 +14,7 @@
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
-
+  struct ctx context;       // thread's context - lab7-1
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
@@ -63,6 +63,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch(&t->context, &current_thread->context);
   } else
     next_thread = 0;
 }
@@ -77,6 +78,9 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  // set thread's function address and thread's stack pointer - lab7-1
+  t->context.ra = (uint64) func;
+  t->context.sp = (uint64) t->stack + STACK_SIZE;
 }
 
 void 
@@ -161,3 +165,23 @@ main(int argc, char *argv[])
   thread_schedule();
   exit(0);
 }
+// Saved registers for thread context switches. - lab7-1
+struct ctx {
+    uint64 ra;
+    uint64 sp;
+
+    // callee-saved
+    uint64 s0;
+    uint64 s1;
+    uint64 s2;
+    uint64 s3;
+    uint64 s4;
+    uint64 s5;
+    uint64 s6;
+    uint64 s7;
+    uint64 s8;
+    uint64 s9;
+    uint64 s10;
+    uint64 s11;
+};
+
