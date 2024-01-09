35abb4ed7a7f38b6c86be5afc3e7bbde3ef34156
lirui <821342264@qq.com>
2024-01-09 22:44:09 +0800

    lab6

--- user/uthread.c
+++ user/uthread.c
@@ -37,7 +37,7 @@ struct thread {
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
-extern void thread_switch(uint64, uint64);
+extern void thread_switch(struct ctx *, struct ctx *);
               
 void 
 thread_init(void)
